24a4e1bb20536c08e4acd271d7dbe3b1662a939b
QYsaltyfish <2291575219@qq.com>
2025-05-29 15:23:47 +0800

    Feat: Implement pdf to markdown convertion function (using magic-pdf).

--- mcp_server/semantic_rag_server.py
+++ mcp_server/semantic_rag_server.py
@@ -1,9 +1,41 @@
 from typing import Any
 import os
 from mcp.server.fastmcp import FastMCP
+import subprocess
+
 
 # Initialize FastMCP server
 mcp = FastMCP("SemanticRAG")
+
+
+def extract_markdown_from_pdf(pdf_path: str) -> str:
+    """
+    Extract Markdown content from a PDF file using the `magic-pdf` CLI tool.
+
+    Args:
+        pdf_path (str): The path to the PDF file to be processed.
+
+    Returns:
+        str: The extracted Markdown content if successful, an error message if the
+             file path is invalid, or if `magic-pdf` fails during execution.
+    """
+
+    if not os.path.exists(pdf_path):
+        return "Invalid PDF path"
+
+    pdf_name = os.path.basename(pdf_path).rstrip(".pdf")
+
+    cmd = ["magic-pdf", "-p", pdf_path, "-o", "output_temp", "-m", "auto"]
+    try:
+        subprocess.run(cmd, check=True)
+
+        with open(f"output_temp/{pdf_name}/auto/{pdf_name}.md", "r") as f:
+            md = f.read()
+
+        return md
+
+    except subprocess.CalledProcessError as e:
+        return f"Error running magic-pdf: {e}"
 
 
 @mcp.tool()
